01a7da8fb51503df73e868116f3f21414e8706c4
nextbase <igor@next-aeon.com>
2015-09-23 10:50:11 +0900

    Added initial template

--- README.md
+++ README.md
@@ -8,4 +8,6 @@ A simple chat api and client written in [Python](http://www.python.org) with [Fl
 
 ### Version -0.10
 + Created the basic structure for the Flask apps
-- Basic folder structure for front-end styling
++ Basic folder structure for front-end styling
++ Install Bourbon, Neat and Zepto.js
+- Create a base HTML template and render the index route
--- static/css/gulpfile.js
+++ static/css/gulpfile.js
@@ -6,23 +6,24 @@ var gutil = require('gulp-util');
 // load plugins
 var $ = require('gulp-load-plugins')();
 
+// compile SASS to CSS
 gulp.task('styles', function () {
     return gulp.src(['sass/main.scss'])
         .pipe($.sass({errLogToConsole: true}))
         .pipe($.autoprefixer('last 1 version'))
-        .pipe(gulp.dest('build'))
+        .pipe(gulp.dest('./'))
         .pipe($.size())
         .pipe($.notify("Compilation complete."));
 });
 
 gulp.task('clean', function () {
-    return gulp.src(['css/main.css', 'dist'], { read: false }).pipe($.clean());
+    return gulp.src(['./main.css', 'dist'], { read: false }).pipe($.clean());
 });
 
+// run to build and quit
 gulp.task('build', ['clean', 'styles']);
 
+// by default gulp will build and watch
 gulp.task('default', ['clean', 'build'], function () {
-    // watch for changes
     gulp.watch('sass/**/*.scss', ['styles']);
 });
-
